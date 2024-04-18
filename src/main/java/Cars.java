a4ac672508aeeff72fa171c6e814b4cc2a6b9d24
asanahmadli <asenahmedli@gmail.com>
2024-04-18 15:43:38 -0400

    I added 2 more class

---
+++ src/main/java/Cars.java
@@ -0,0 +1,91 @@
+public class Cars {
+/**The aim of this tutorial is to manage the access of a table in database
+ * from separate layer written in java, this layer usually called Data Access Layer (DAL)
+ *
+ * The most benefit from DAL is that it simplifies database access operations through some method calls like insert() and find()
+ *rather than making connection and executing some queries
+ *
+ * this layer instead handles all database related calls and queries inside it.
+ * every column in the table would be a member variable in the class.
+ *
+ * our aim is to order this layer to create, modify, delete or search for an entity
+ * Using simple java objects rather than dealing with SQL statements and other database related commands.
+ * A good practice is to provide default empty constructor, a full constructor and a full constructor without the id parameter.
+ * */
+
+private Integer id;
+private String make;
+private String model;
+private String year;
+private String color;
+private String vin;
+
+public Cars(){
+
+}
+
+    public Cars(Integer id, String make, String model, String year, String color, String vin) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.vin = vin;
+    }
+
+    public Cars(String make, String model, String year, String color, String vin) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.vin = vin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+}//class
---
+++ src/main/java/ConnectionFactory.java
@@ -0,0 +1,35 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class ConnectionFactory {
+    public static final String URL = "jdbc:mysql://localhost:3306/university";
+    public static final String USER = "asan";
+    public static final String PASS = "pass";
+    public static String query = "select * from EngineeringStudents where Last_Name = 'Pujar';";
+    public static Connection getConnection(){
+        try {
+            DriverManager.registerDriver(new Driver());
+            return  DriverManager.getConnection(URL,USER,PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database",e);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = ConnectionFactory.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()){
+            String universityData = "";
+            for (int i = 1; i<=6;i++){
+                universityData += resultSet.getString(i) + ":";
+            }
+            System.out.println(universityData);
+        }
+
+
+    }//main
+
+}//class
